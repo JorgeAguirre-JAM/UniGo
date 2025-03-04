713add92f6b65ed2b9b702a3651aa9a97079bd6a
JorgeAguirre <aguirreayalajorgemanuel@gmail.com>
2025-03-03 21:33:09 -0600

    nuevaseccvopn

--- js/script.js
+++ js/script.js
@@ -18,16 +18,16 @@ function loadShow() {
         stt++;
         items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
         items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        items[i].style.filter = 'blur(0px)';
+        items[i].style.opacity = stt > 2 ? 0 : 10;
     }
     stt = 0;
     for (var i = active - 1; i >= 0; i--) {
         stt++;
         items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
         items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        items[i].style.filter = 'blur(0px)';
+        items[i].style.opacity = stt > 2 ? 0 : 100;
     }
 }
 loadShow();
